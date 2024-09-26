63805add11be6e918565b6894a0f663e7a4a730b
jyhgyh <rinat.hirfanov@epitech.eu>
2024-09-26 12:47:12 +0300

    battleship game finished

--- Battleship/battleship.php
+++ Battleship/battleship.php
@@ -1,33 +1,14 @@
 <?php
-
-function colle($x, $y, array $ships = [])
+function displayGrid($grid, $x, $y)
 {
-    if ($x < 1 || $y < 1) {
-        return;
-    }
-
-    $grit = [];
-    for ($i = 0; $i < $y; $i++) {
-        $grit[$i] = array_fill(0, $x, " ");
-    }
-
-    foreach ($ships as $pos) {
-        $xx = $pos[0];
-        $yy = $pos[1];
-        if ($xx >= 0 && $xx < $x && $yy >= 0 && $yy < $y) {
-            $grit[$yy][$xx] = "X";
-        }
-    }
-
     for ($row = 0; $row < $y; $row++) {
         echo "+";
         for ($col = 0; $col < $x; $col++) {
             echo "---+";
         }
-        echo "\n";
-        echo "|";
+        echo "\n|";
         for ($col = 0; $col < $x; $col++) {
-            echo " " . $grit[$row][$col] . " |";
+            echo " " . $grid[$row][$col] . " |";
         }
         echo "\n";
     }
@@ -38,4 +19,104 @@ function colle($x, $y, array $ships = [])
     echo "\n";
 }
 
-colle(5, 3, [[0, 1], [2, 1], [1, 2], [1, 7]]);
+function initGrid($x, $y)
+{
+    $grid = [];
+    for ($i = 0; $i < $y; $i++) {
+        $grid[$i] = array_fill(0, $x, " ");
+    }
+    return $grid;
+}
+
+function addShip(&$grid, $x, $y, $posX, $posY)
+{
+    if ($posX >= 0 && $posX < $x && $posY >= 0 && $posY < $y) {
+        if ($grid[$posY][$posX] == "X") {
+            echo "The ship has already been placed in this position.\n";
+        } else {
+            $grid[$posY][$posX] = "X";
+            echo "The ship is positioned [$posX, $posY].\n";
+        }
+    } else {
+        echo "Incorrect coordinates.\n";
+    }
+}
+
+function attack(&$grid, $posX, $posY)
+{
+    if ($grid[$posY][$posX] == "X") {
+        echo "It's a hit! The ship is sunk.\n";
+        $grid[$posY][$posX] = "O";
+        return true;
+    } else {
+        echo "Missed.\n";
+        return false;
+    }
+}
+
+function hasShips($grid)
+{
+    foreach ($grid as $row) {
+        if (in_array("X", $row)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function battleshipGame()
+{
+    $x = 5;
+    $y = 5;
+    $gridPlayer1 = initGrid($x, $y);
+    $gridPlayer2 = initGrid($x, $y);
+
+    echo "Player 1, position your ships:\n";
+    for ($i = 1; $i <= 2; $i++) {
+        $posX = (int)readline("Enter the X coordinate for the ship $i: ");
+        $posY = (int)readline("Enter the Y coordinate for the ship $i: ");
+        addShip($gridPlayer1, $x, $y, $posX, $posY);
+    }
+
+    echo "Player 2, position your ships:\n";
+    for ($i = 1; $i <= 2; $i++) {
+        $posX = (int)readline("Enter the X coordinate for the ship $i: ");
+        $posY = (int)readline("Enter the Y coordinate for the ship $i: ");
+        addShip($gridPlayer2, $x, $y, $posX, $posY);
+    }
+
+    $turn = 1;
+    while (true) {
+        echo "\nPlayer Field 1:\n";
+        displayGrid($gridPlayer1, $x, $y);
+
+        echo "\nПоле игрока 2:\n";
+        displayGrid($gridPlayer2, $x, $y);
+
+        if ($turn == 1) {
+            echo "Player 1's move:\n";
+            $posX = (int)readline("Enter the X coordinate for the attack: ");
+            $posY = (int)readline("Enter the Y coordinate for the attack: ");
+            if (attack($gridPlayer2, $posX, $posY)) {
+                if (!hasShips($gridPlayer2)) {
+                    echo "Player 1 wins!\n";
+                    break;
+                }
+            }
+            $turn = 2;
+        } else {
+            echo "Player's move 2:\n";
+            $posX = (int)readline("Enter the X coordinate for the attack: ");
+            $posY = (int)readline("Enter the Y coordinate for the attack: ");
+            if (attack($gridPlayer1, $posX, $posY)) {
+                if (!hasShips($gridPlayer1)) {
+                    echo "Player 2 wins!\n";
+                    break;
+                }
+            }
+            $turn = 1;
+        }
+    }
+}
+
+battleshipGame();
